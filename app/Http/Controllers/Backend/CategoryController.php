5b446121abb2b5acb2369c13c5d770903fa03959
Nurunnahar <nurunnahar9081@gmail.com>
2024-10-22 11:04:11 +0600

    category create and update done

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -3,13 +3,103 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Models\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 
 class CategoryController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $categories = Category::orderBy('id', 'desc')->paginate(10);
+
         return view('admin.category.index', compact('categories'));
+    }
+
+    public function createCategory()
+    {
+        return view('admin.category.create');
+    }
+
+    public function storeCategory(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'image' => 'nullable|image|max:1024',
+        ]);
+
+        if ($request->hasFile('image')) {
+
+            $file = request()->file('image');
+            $fileName = hexdec(uniqid()).'.'.$file->getClientOriginalExtension(); //for name making
+            $file->move(public_path('/uploads/category'), $fileName);
+            $filePath = "uploads/category/{$fileName}";
+
+            Category::create([
+                'name' => $request->name,
+                'slug' => Str::slug($request->name),
+                'image' => $filePath,
+            ]);
+
+            return redirect()->route('admin.category.index')->with('status', 'Category successfully created.');
+        }
+        Category::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ]);
+
+        return redirect()->route('admin.category.index')->with('status', 'Category successfully created.');
+
+    }
+
+    public function editCategory($slug)
+    {
+        $categories = Category::where('slug', $slug)->first();
+
+        return view('admin.category.edit', compact('categories'));
+    }
+
+    public function updateCategory(Request $request, $slug)
+    {
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'image' => 'nullable|image|max:1024',
+        ]);
+
+        // Find the category instance by slug
+        $category = Category::where('slug', $slug)->firstOrFail();
+
+        // Check if an image is uploaded
+        if ($request->hasFile('image')) {
+
+            // Delete previous image if it exists
+            if ($category && File::exists(public_path($category->image))) {
+                File::delete(public_path($category->image));
+            }
+
+            // Process and upload the new image
+            $file = $request->file('image');
+            $fileName = hexdec(uniqid()).'.'.$file->getClientOriginalExtension();
+            $file->move(public_path('/uploads/category'), $fileName);
+            $filePath = "uploads/category/{$fileName}";
+
+            // Update the category instance with image
+            $category->update([
+                'name' => $request->name,
+                'slug' => Str::slug($request->name),
+                'image' => $filePath,
+            ]);
+
+            return redirect()->route('admin.category.index')->with('status', 'Category successfully updated.');
+        }
+
+        // Update the category without image
+        $category->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name),
+        ]);
+
+        return redirect()->route('admin.category.index')->with('status', 'Category successfully updated.');
     }
 }
---
+++ resources/views/admin/category/edit.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.admin')
+
+@section('admin_content')
+    <div class="main-content">
+        <div class="main-content-inner">
+            <div class="main-content-wrap">
+                <div class="flex items-center flex-wrap justify-between gap20 mb-27">
+                    <h3>Category infomation</h3>
+                    <ul class="breadcrumbs flex items-center flex-wrap justify-start gap10">
+                        <li>
+                            <a href="{{ route('admin.index') }}">
+                                <div class="text-tiny">Dashboard</div>
+                            </a>
+                        </li>
+                        <li>
+                            <i class="icon-chevron-right"></i>
+                        </li>
+                        <li>
+                            <a href="{{ route('admin.category.index') }}">
+                                <div class="text-tiny">Category</div>
+                            </a>
+                        </li>
+                        <li>
+                            <i class="icon-chevron-right"></i>
+                        </li>
+                        <li>
+                            <div class="text-tiny">New Category</div>
+                        </li>
+                    </ul>
+                </div>
+                <!-- new-category -->
+                <div class="wg-box">
+                    <form class="form-new-product form-style-1" action="{{ route('admin.category.update', $categories->slug) }}" method="POST"
+                        enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <fieldset class="name">
+                            <div class="body-title">Category Name <span class="tf-color-1">*</span></div>
+                            <input class="flex-grow @error('name') is-invalid @enderror" type="text"
+                                placeholder="Brand name" name="name" tabindex="0" value="{{ $categories->name }}"
+                                aria-required="true" required="">
+                            @error('name')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </fieldset>
+                     
+                        <fieldset>
+                            <div class="body-title">Upload images <span class="tf-color-1">*</span>
+                            </div>
+                           <div class="mb-3">
+                                <label class="form-label">Category image</label>
+                                <input oninput="newImg.src=window.URL.createObjectURL(this.files[0])" class="form-control"
+                                name="image" type="file" id="image">
+                            </div>
+
+                             <div class="mb-3">
+                                <img class="" id="newImg" width="150" src="{{ asset($categories->image ?? '') }}">
+                            </div>
+                        </fieldset>
+
+                        <div class="bot">
+                            <div></div>
+                            <button class="tf-button w208" type="submit">Save</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="bottom-page">
+            <div class="body-text">Copyright © 2024</div>
+        </div>
+    </div>
+@endsection
